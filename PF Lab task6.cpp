44e9f0bf58aa5faae0c010ce521e6691ee2290a6
Ayesha-Hayat26K0607 <bintehayat246@gmail.com>
2026-09-08 13:30:58 +0500

    Add files via upload
    PF Lab tasks

---
+++ PF Lab task6.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+	int month,year;
+	printf("Enter a number (1 to 12) : \n" );
+	scanf("%d",&month);
+	printf("Enter a year : \n");
+	scanf("%d",&year);
+	switch(month)
+	{
+	case 1:
+		printf("January %d has 31 days",year);
+		break;
+	case 2:
+		if(year%4==0 && year%100!=0|| year%400==0)
+		   printf("February %d has 29 days",year);
+		else
+		printf("February %d has 28 days",year);
+		break;
+	case 3:
+		printf("March %d has 31 days",year);
+		break;
+	case 4:
+		printf("April %d has 30 days",year);
+		break;
+	case 5:
+		printf("May %d has 31 days",year);
+		break;
+	case 6:
+		printf("June %d has 30 days",year);
+		break;
+	case 7:
+		printf("July %d has 31 days",year);
+		break;
+	case 8:
+		printf("August %d has 31 days",year);
+		break;
+	case 9:
+		printf("September %d has 30 days",year);
+		break;
+	case 10:
+		printf("October %d has 31 days",year);
+		break;
+	case 11:
+		printf("November %d has 30 days",year);
+		break;
+	case 12:
+		printf("December %d has 31 days",year);
+		break;
+    }
+     return 0;
+}
